785c8361d82fdc06532729aea7d24103a48ba709
Khalouna TRAN <khtran@e1r8p9.42.fr>
2019-05-13 13:45:00 +0200

    Bonne version du corewar

--- corewar/Corewar-asm/test.s
+++
@@ -1,8 +0,0 @@
-.name		"test"
-.comment	"test instructions"
-
-live: live %42 ,  :label ,  26
-live %:live1
-lfork %:live
-aff r2
-live1: 
--- corewar/Corewar-asm/vm_champs/champs/Car.s
+++
@@ -1,6 +0,0 @@
-#deassembler par msa 1.8, made by joe
-.name "Tching tching(Intercepteur), Bouh!Bouh!(bruits d'anti-jeu)"
-.comment ""
-
-	 ld    %-272,r3           		# 7,3,278
-
